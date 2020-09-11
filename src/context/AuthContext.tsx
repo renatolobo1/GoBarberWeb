1498d672ac5a4bc6cbb06d1b45c49407aa6a6ed8
Renatolobo1 <renatolobo_@hotmail.com>
2020-09-11 17:03:40 -0300

    Criando hook useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useCallback, useState} from 'react';
+import React, {createContext, useCallback, useState, useContext} from 'react';
 import api from '../services/api';
 
 interface AuthState{
@@ -17,7 +17,7 @@ interface AuthContextData {
 
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
   const[data, setData] = useState<AuthState >(()=>{
@@ -51,4 +51,14 @@ export const AuthProvider: React.FC = ({children}) => {
   </AuthContext.Provider>
   );
 };
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context){
+    throw new Error('useAuth must be used witihin as AuthProvider')
+  }
+
+  return context;
+}
 
